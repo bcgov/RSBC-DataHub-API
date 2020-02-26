cb2d29299709b68c65c38e23e79c52259104dc78
Jonathan Longe <jlonge@corebox.net>
2020-02-26 15:59:36 -0800

    modify validator to make it more fault tolerant

--- validator/config.py
+++ validator/config.py
@@ -7,18 +7,11 @@ class Config():
 
     RABBITMQ_URL                = os.getenv('RABBITMQ_URL', 'localhost')
     RABBITMQ_WATCH_QUEUE        = os.getenv('RABBITMQ_WATCH_QUEUE', 'ingested')
-    RABBITMQ_MISC_ERROR_QUEUE   = os.getenv('RABBITMQ_VALID_QUEUE', 'misc.error')
+    RABBITMQ_VALID_QUEUE        = os.getenv('RABBITMQ_VALID_QUEUE', 'ETK.valid')
+    RABBITMQ_FAIL_QUEUE         = os.getenv('RABBITMQ_FAIL_QUEUE', 'ETK.not-valid')
     RABBITMQ_MESSAGE_ENCODE     = os.getenv('RABBITMQ_MESSAGE_ENCODE', 'utf-8')
 
     SCHEMA_FILENAME             = os.getenv('SCHEMA_FILENAME', 'schemas.json')
 
     VALIDATOR_USER              = os.getenv('VALIDATOR_USER')
     VALIDATOR_PASS              = os.getenv('VALIDATOR_PASS')
-
-    # The validator automatically creates destination valid and not valid queues
-    # as needed.  The destination queues follow the format 'ETK.valid' and 
-    # 'ETK.not-valid' where 'ETK' is taken from the 'event_source' attribute in 
-    # the message but can be configured to any message attribute by overriding 
-    # the 'MESSAGE_QUEUE_ATTRIBUTE' variable below.
-    MESSAGE_QUEUE_ATTRIBUTE     = os.getenv('MESSAGE_QUEUE_ATTRIBUTE', 'event_source')
-
--- validator/listener.py
+++ validator/listener.py
@@ -16,32 +16,31 @@ class Listener():
         parameters = pika.URLParameters(url)
         self.connection = pika.BlockingConnection(parameters)
         self.validator = validator
-        self.availableQueues = []
-        self.messageAttributeUsedForQueueName = config.MESSAGE_QUEUE_ATTRIBUTE
+        self.config = config
         logging.warning('*** validator initialized  ***')
 
     def main(self):
         channel = self.connection.channel()
         channel.basic_qos(prefetch_count=1)
-        channel.basic_consume(queue=Config.RABBITMQ_WATCH_QUEUE, on_message_callback=self.callback)
+        channel.basic_consume(queue=self.config.RABBITMQ_WATCH_QUEUE, on_message_callback=self.callback)
         channel.start_consuming()
         logging.warning('*** validator listening  ***')
 
 
     def callback(self, ch, method, properties, body):
         # convert body (in bytes) to string 
-        message = body.decode(Config.RABBITMQ_MESSAGE_ENCODE)
+        message = body.decode(self.config.RABBITMQ_MESSAGE_ENCODE)
         messageDict = json.loads(message)
         
-        queue = self._getWriteQueue(
-            self.validator.validate(messageDict),
-            messageDict,
-            self.messageAttributeUsedForQueueName)
-            
+        if(self.validator.validate(messageDict)):
+            queue = self.config.RABBITMQ_VALID_QUEUE
+        else:
+            queue = self.config.RABBITMQ_FAIL_QUEUE
+
         logging.warning("write to: " + queue)
 
-        # only remove the message from the ingested queue if
-        # if it's successfully been writen to another queue
+        # only remove the message from the ingested queue if it has 
+        # successfully been writen to a `valid` or `not-valid` queue
         if(self._publish(ch, queue, message )):
             ch.basic_ack(delivery_tag=method.delivery_tag)
 
@@ -51,48 +50,20 @@ class Listener():
 
 
     def _publish(self, channel, queue_name, message):
-        self._createQueueIfNotExists(channel, self.availableQueues, queue_name)
+    
         tries = Listener.maximumConnectionRetries 
         while tries > 0:
             tries -= 1;
 
             try:
-                channel.basic_publish( exchange='', routing_key=queue_name, body=message)
+                channel.basic_publish( exchange='', routing_key=queue_name, body=message, mandatory=True)
                 return True
 
             except Exception as error:
-                logging.warning(error)
+                logging.warning("Could not publish message to queue ... trying to reestablish the connection")
         
         return False
-        
 
-    def _getWriteQueue(self, isValid: bool, message: dict, messageAttribute: str ):
-
-        if messageAttribute not in message:
-            # if the message_type attribute doesn't exist in the
-            # message, write the message to a miscelllaneous error queue
-            return Config.RABBITMQ_MISC_ERROR_QUEUE
-
-        # otherwise write the message to an event-type specific queue
-        if(isValid):
-            return message[messageAttribute] + ".valid"
-        else:
-            return message[messageAttribute] + ".not-valid"
-
-
-    def _createQueueIfNotExists(self, channel, queuesAlreadyDeclared, currentQueue):
-        if currentQueue not in queuesAlreadyDeclared:
-            logging.warning('creating a new queue: ' + currentQueue)
-            channel.queue_declare(queue=currentQueue, durable=True)
-            self.availableQueues.append(currentQueue)
-
-        return
-
-        
-
-        
-
-       
 
         
 if __name__ == "__main__":
