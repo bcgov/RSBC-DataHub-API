7fec6a34faefbbc74bf7fcdb9c4d8703df8933ce
Jonathan Longe <jlonge@corebox.net>
2020-10-28 14:43:58 -0700

    display 'at 9:30am' for all written review time slots

--- python/common/vips_api.py
+++ python/common/vips_api.py
@@ -262,8 +262,8 @@ def time_slot_to_friendly_string(time_slot: dict, presentation_type: str) -> dic
             vips_str_to_friendly_time(start_time),
             vips_str_to_friendly_time(end_time))
     elif presentation_type == "WRIT":
-        label = "{}".format(
-            # Friday, Sept 4, 2020
+        label = "{} at 9:30am".format(
+            # Friday, Sept 4, 2020 at 9:30am
             vips_str_to_datetime(start_time).strftime("%a, %b %-d, %Y"))
     return {
         "label": label,
