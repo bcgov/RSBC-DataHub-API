3baf663c209b62be926875822547568451718e56
Jonathan Longe <jlonge@corebox.net>
2020-12-11 17:52:32 -0800

    improve paybc search() tests

--- python/tests/test_payment.py
+++ python/tests/test_payment.py
@@ -2,64 +2,169 @@ import pytest
 import pytz
 import datetime
 import logging
+import base64
 import python.common.helper as helper
 import python.common.middleware as middleware
 import python.paybc_api.business as business
 from python.paybc_api.website.config import Config
+import python.paybc_api.website.app as app
 import python.common.vips_api as vips
 
 
-test_search_method_data = [
-    # Type, ServiceDate   TodayIs       Paid     LastName  InVips  Error
-    ('IRP', "2020-09-10", "2020-09-15", False,   "Norris", True,  "The last name doesn't match a driving prohibition in the system."),
-    ('IRP', "2020-09-10", "2020-09-18", False,   "Gordon", True,  "The Notice of Prohibition was issued more than 7 days ago."),
-    ('IRP', "2020-09-10", "2020-09-16", False,   "Gordon", True,  None),
-    ('IRP', "2020-09-10", "2020-09-14", False,   "Gordon", False, "The driving prohibition isn't in the system."),
-    ('IRP', "2020-09-10", "2020-09-14", True,    "Gordon", True,  "The application review fee has already been paid."),
-    ('IRP', "2020-09-10", "2020-09-14", False,   "Gordon", True,  None),
-
-    ('ADP', "2020-09-10", "2020-09-15", False,   "Norris", True,  "The last name doesn't match a driving prohibition in the system."),
-    ('ADP', "2020-09-10", "2020-09-18", False,   "Gordon", True,  "The Notice of Prohibition was issued more than 7 days ago."),
-    ('ADP', "2020-09-10", "2020-09-16", False,   "Gordon", True,  None),
-    ('ADP', "2020-09-10", "2020-09-14", False,   "Gordon", False, "The driving prohibition isn't in the system."),
-    ('ADP', "2020-09-10", "2020-09-14", True,    "Gordon", True,  "The application review fee has already been paid."),
-    ('ADP', "2020-09-10", "2020-09-14", False,   "Gordon", True,  None),
-
-    ('UL',  "2020-09-10", "2020-09-15", False,   "Norris", True,  "The last name doesn't match a driving prohibition in the system."),
-    ('UL',  "2020-09-10", "2020-09-14", False,   "Gordon", False, "The driving prohibition isn't in the system."),
-    ('UL',  "2020-09-10", "2020-09-14", True,    "Gordon", True,  "The application review fee has already been paid."),
-    ('UL',  "2020-09-10", "2020-09-14", False,   "Gordon", True,  None),
-]
+@pytest.fixture
+def client(monkeypatch):
+    monkeypatch.setattr(Config, "PAYBC_CLIENT_ID", "client-id")
+    monkeypatch.setattr(Config, "PAYBC_CLIENT_SECRET", "secret")
+    monkeypatch.setenv("AUTHLIB_INSECURE_TRANSPORT", '1')
+    monkeypatch.setattr(Config, "ABSOLUTE_DB_PATH", "/tmp")
+    application = app.create_app(Config)
+    application.testing = True
+    with application.test_client() as client:
+        yield client
 
 
-@pytest.mark.parametrize(
-    "prohibition_type, date_served, today_is, is_paid, last_name, is_in_vips, error", test_search_method_data)
-def test_search_method(
-        prohibition_type, date_served, today_is, is_paid, last_name, is_in_vips, error, monkeypatch):
+@pytest.fixture
+def token(client, monkeypatch):
+    headers = get_basic_authentication_header(Config)
+    logging.warning(headers)
+    response = client.post('/oauth/token', data={'grant_type': 'client_credentials'}, headers=headers)
+    token = response.json['access_token']
+    logging.warning("token {}".format(token))
+    return token
+
+
+def test_get_token_endpoint(client, monkeypatch):
+    headers = get_basic_authentication_header(Config)
+    logging.warning(headers)
+    response = client.post('/oauth/token', data={'grant_type': 'client_credentials'}, headers=headers)
+    logging.warning(response.json)
+    assert response.status_code == 200
+
+
+def test_search_endpoint_requires_an_access_token(token, client, monkeypatch):
+    response = client.get('/api_v2/search', query_string=get_search_payload())
+    logging.warning("search: {}".format(response.json))
+    assert response.status_code == 401
+
+
+@pytest.mark.parametrize("prohibition_types", ['IRP', 'ADP', 'UL'])
+def test_applicant_search_returns_error_if_entered_last_name_incorrect(prohibition_types, token, client, monkeypatch):
 
     def mock_status_get(*args, **kwargs):
-        print('inside mock_status_get()')
-        return status_get(is_in_vips, prohibition_type, date_served, last_name, is_paid, True)
+        return status_get(True, prohibition_types, "2020-09-01", "Wrong-Name", True, True)
 
-    def mock_datetime_now(**args):
-        args['today_date'] = helper.localize_timezone(datetime.datetime.strptime(today_is, "%Y-%m-%d"))
-        print('inside mock_datetime_now: {}'.format(args.get('today_date')))
-        return True, args
-
-    monkeypatch.setattr(middleware, "determine_current_datetime", mock_datetime_now)
     monkeypatch.setattr(vips, "status_get", mock_status_get)
 
-    results = helper.middle_logic(business.search_for_invoice(),
-                                  prohibition_number="20-123456",
-                                  driver_last_name="Gordon",
-                                  config=Config)
+    response = client.get('/api_v2/search', query_string=get_search_payload(), headers=get_oauth_auth_header(token))
+    logging.warning("search: {}".format(response.json))
+    assert "The last name doesn't match a driving prohibition in the system." in response.json['error']
+    assert response.status_code == 200
 
-    print(results.get('error_string'))
-    if error is None:
-        assert "error_string" not in results
-    else:
-        assert "error_string" in results
-        assert results.get('error_string') == error
+
+@pytest.mark.parametrize("prohibition_types", ['IRP', 'ADP', 'UL'])
+def test_applicant_search_returns_error_if_already_paid(prohibition_types, token, client, monkeypatch):
+
+    def mock_status_get(*args, **kwargs):
+        return status_get(True, prohibition_types, "2020-09-01", "Gordon", True, True)
+
+    monkeypatch.setattr(vips, "status_get", mock_status_get)
+
+    response = client.get('/api_v2/search', query_string=get_search_payload(), headers=get_oauth_auth_header(token))
+    logging.warning("search: {}".format(response.json))
+    assert "The application review fee has already been paid." in response.json['error']
+    assert response.status_code == 200
+
+
+@pytest.mark.parametrize("prohibition_types", ['IRP', 'ADP', 'UL'])
+def test_applicant_search_returns_error_if_not_found_in_vips(prohibition_types, token, client, monkeypatch):
+
+    def mock_status_get(*args, **kwargs):
+        return status_get(False, prohibition_types, "2020-09-01", "Gordon", True, True)
+
+    monkeypatch.setattr(vips, "status_get", mock_status_get)
+
+    response = client.get('/api_v2/search', query_string=get_search_payload(), headers=get_oauth_auth_header(token))
+    logging.warning("search: {}".format(response.json))
+    assert "The driving prohibition isn't in the system." in response.json['error']
+    assert response.status_code == 200
+
+
+@pytest.mark.parametrize("prohibition_type", ['IRP', 'ADP'])
+def test_applicant_search_returns_error_if_irp_or_adp_older_than_7_days(prohibition_type, token, client, monkeypatch):
+    iso_format = "%Y-%m-%d"
+    tz = pytz.timezone('America/Vancouver')
+    date_served = (datetime.datetime.now(tz) - datetime.timedelta(days=8)).strftime(iso_format)
+
+    def mock_status_get(*args, **kwargs):
+        return status_get(True, prohibition_type, date_served, "Gordon", False, True)
+
+    monkeypatch.setattr(vips, "status_get", mock_status_get)
+
+    response = client.get('/api_v2/search', query_string=get_search_payload(), headers=get_oauth_auth_header(token))
+    logging.warning("search: {}".format(response.json))
+    assert "The Notice of Prohibition was issued more than 7 days ago." in response.json['error']
+    assert response.status_code == 200
+
+
+def test_applicant_search_successful_if_a_ul_review_older_than_7_days(token, client, monkeypatch):
+    iso_format = "%Y-%m-%d"
+    tz = pytz.timezone('America/Vancouver')
+    date_served = (datetime.datetime.now(tz) - datetime.timedelta(days=8)).strftime(iso_format)
+
+    def mock_status_get(*args, **kwargs):
+        return status_get(True, "UL", date_served, "Gordon", False, True)
+
+    monkeypatch.setattr(vips, "status_get", mock_status_get)
+
+    response = client.get('/api_v2/search', query_string=get_search_payload(), headers=get_oauth_auth_header(token))
+    logging.warning("search: {}".format(response.json))
+    assert "error" not in response.json
+    assert "https://localhost/api_v2/invoice/20123456" in response.json['items'][0]['selected_invoice']['$ref']
+    assert response.status_code == 200
+
+
+@pytest.mark.parametrize("prohibition_number", ['20123456', '20123457'])
+def test_successful_search_response_includes_url_to_invoice_endpoint(prohibition_number, token, client, monkeypatch):
+    iso_format = "%Y-%m-%d"
+    tz = pytz.timezone('America/Vancouver')
+    date_served = (datetime.datetime.now(tz) - datetime.timedelta(days=6)).strftime(iso_format)
+
+    def mock_status_get(*args, **kwargs):
+        return status_get(True, "IRP", date_served, "Gordon", False, True)
+
+    monkeypatch.setattr(vips, "status_get", mock_status_get)
+
+    response = client.get('/api_v2/search',
+                          query_string=get_search_payload(prohibition_number),
+                          headers=get_oauth_auth_header(token))
+    logging.warning("search: {}".format(response.json))
+    endpoint = "https://localhost/api_v2/invoice/{}".format(prohibition_number)
+    assert "error" not in response.json
+    assert endpoint in response.json['items'][0]['selected_invoice']['$ref']
+    assert response.status_code == 200
+
+
+def get_basic_authentication_header(config) -> dict:
+    username = config.PAYBC_CLIENT_ID
+    password = config.PAYBC_CLIENT_SECRET
+    credentials = base64.b64encode((username + ":" + password).encode('utf-8')).decode('utf-8')
+    headers = dict({
+        'Authorization': 'Basic {}'.format(credentials),
+        'Content-Type': 'application/x-www-form-urlencoded'
+    })
+    logging.debug(headers)
+    return headers
+
+
+def get_search_payload(prohibition_number="20-123456", last_name="Gordon") -> dict:
+    return dict({
+        "invoice_number": prohibition_number,
+        "check_value": last_name
+    })
+
+
+def get_oauth_auth_header(token) -> dict:
+    return dict({"Authorization": "Bearer {}".format(token)})
 
 
 test_show_method_data = [
@@ -108,97 +213,7 @@ def test_pay_bc_generate_invoice_method(prohibition_type, application_created, e
         assert results.get('error_string') == error
 
 
-def test_an_applicant_must_apply_for_a_review_before_paying_the_application_fee(monkeypatch):
-    is_in_vips = True
-    prohibition_type = "ADP"
-    last_name = "Gordon"
-    date_served = datetime.datetime.now().strftime("%Y-%m-%d")
-    application_created = False
-    is_paid = False
-    expected_error = "You must submit an application before you can pay."
 
-    def mock_status_get(*args, **kwargs):
-        return status_get(is_in_vips, prohibition_type, date_served, last_name, is_paid, application_created)
-
-    monkeypatch.setattr(vips, "status_get", mock_status_get)
-
-    results = helper.middle_logic(business.search_for_invoice(),
-                                  driver_last_name="Gordon",
-                                  prohibition_number="20-123456",
-                                  config=Config)
-    error = results.get('error_string')
-    logging.warning(error)
-    assert error == expected_error
-
-
-def test_an_applicant_must_enter_a_last_name_that_matches_vips(monkeypatch):
-    is_in_vips = True
-    prohibition_type = "ADP"
-    last_name = "Smith"
-    date_served = "2020-09-10"
-    application_created = True
-    is_paid = False
-    expected_error = "The last name doesn't match a driving prohibition in the system."
-
-    def mock_status_get(*args, **kwargs):
-        return status_get(is_in_vips, prohibition_type, date_served, last_name, is_paid, application_created)
-
-    monkeypatch.setattr(vips, "status_get", mock_status_get)
-
-    results = helper.middle_logic(business.search_for_invoice(),
-                                  driver_last_name="Gordon",
-                                  prohibition_number="20-123456",
-                                  config=Config)
-    error = results.get('error_string')
-    logging.warning(error)
-    assert error == expected_error
-
-
-def test_an_applicant_must_pay_before_application_window_has_expired(monkeypatch):
-    is_in_vips = True
-    prohibition_type = "IRP"
-    last_name = "Gordon"
-    tz = pytz.timezone('America/Vancouver')
-    date_served = (datetime.datetime.now(tz) - datetime.timedelta(days=8)).strftime("%Y-%m-%d")
-    application_created = True
-    is_paid = False
-    expected_error = "The Notice of Prohibition was issued more than 7 days ago."
-
-    def mock_status_get(*args, **kwargs):
-        return status_get(is_in_vips, prohibition_type, date_served, last_name, is_paid, application_created)
-
-    monkeypatch.setattr(vips, "status_get", mock_status_get)
-
-    results = helper.middle_logic(business.search_for_invoice(),
-                                  driver_last_name="Gordon",
-                                  prohibition_number="20-123456",
-                                  config=Config)
-    error = results.get('error_string')
-    logging.warning(error)
-    assert error == expected_error
-
-
-def test_an_applicant_must_not_have_paid_previously(monkeypatch):
-    is_in_vips = True
-    prohibition_type = "IRP"
-    last_name = "Gordon"
-    date_served = datetime.datetime.now().strftime("%Y-%m-%d")
-    application_created = True
-    is_paid = True
-    expected_error = "The application review fee has already been paid."
-
-    def mock_status_get(*args, **kwargs):
-        return status_get(is_in_vips, prohibition_type, date_served, last_name, is_paid, application_created)
-
-    monkeypatch.setattr(vips, "status_get", mock_status_get)
-
-    results = helper.middle_logic(business.search_for_invoice(),
-                                  driver_last_name="Gordon",
-                                  prohibition_number="20-123456",
-                                  config=Config)
-    error = results.get('error_string')
-    logging.warning(error)
-    assert error == expected_error
 
 
 def status_get(is_success, prohibition_type, date_served, last_name, is_paid, application_saved):
