39b241e4224bbeec8deeaed82755658bd2d36f23
Jonathan Longe <jlonge@corebox.net>
2020-12-20 16:43:16 -0800

    query_for_additional_review_times() not incrementing bus. days

--- python/common/middleware.py
+++ python/common/middleware.py
@@ -363,13 +363,14 @@ def query_for_additional_review_times(**args) -> tuple:
     time_slots = args.get('time_slots')
     last_date = args.get('max_review_date')
     review_type = args.get('presentation_type')
-    while number_review_days_offered < config.MIN_REVIEW_DAYS_OFFERED and query_count <= config.ADDITIONAL_DAYS_TO_QUERY:
+    while number_review_days_offered < config.MIN_REVIEW_DAYS_OFFERED and query_count < config.ADDITIONAL_DAYS_TO_QUERY:
         logging.info("Querying for additional review dates. Number offered so far: {}".format(number_review_days_offered))
         datetime_to_query = vips.next_business_date(last_date)
         query_count += 1
         is_successful, data = vips.schedule_get(
             vips_data['noticeTypeCd'],
             review_type, datetime_to_query, datetime_to_query, config, correlation_id)
+        last_date = datetime_to_query
         if len(data["time_slots"]) > 0:
             number_review_days_offered += 1
             time_slots.append(data['time_slots'][0])
