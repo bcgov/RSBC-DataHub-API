89b6bc14e0ca622441352d1fb3b04a746a3d7871
Jonathan Longe <jlonge@corebox.net>
2022-02-01 18:34:42 -0800

    respond to '/roadside-forms/' v0.1.83

---
+++ web_app/dist/precache-manifest.d27947b45699fdb596f8da8a0be1c032.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a1d1387d63bfa8f038a3563072d61e5",
+    "url": "/roadside-forms/assets/BCID_RoadSafetyBC_logo_transparent.png"
+  },
+  {
+    "revision": "b317037f4b60423673d540d2b2ef33b6",
+    "url": "/roadside-forms/assets/MV2634_102018_driver.png"
+  },
+  {
+    "revision": "01c58e4363c8d6d8cb0cadb64a913c99",
+    "url": "/roadside-forms/assets/MV2634_102018_police.png"
+  },
+  {
+    "revision": "025369f886084a9965f2dcdd2418e4c3",
+    "url": "/roadside-forms/assets/MV2906_102018.png"
+  },
+  {
+    "revision": "5b1f38fedfac95103b00",
+    "url": "/roadside-forms/css/app.c89772f4.css"
+  },
+  {
+    "revision": "983d921f6823f89ae478",
+    "url": "/roadside-forms/css/chunk-vendors.e42a3c01.css"
+  },
+  {
+    "revision": "dd8db4aa58a73c03acd0baaab69636f2",
+    "url": "/roadside-forms/index.html"
+  },
+  {
+    "revision": "5b1f38fedfac95103b00",
+    "url": "/roadside-forms/js/app.add5cc4b.js"
+  },
+  {
+    "revision": "4371f7fa8a1080647f49",
+    "url": "/roadside-forms/js/chunk-2d216214.02f52b64.js"
+  },
+  {
+    "revision": "63c4ef073f721ab82397",
+    "url": "/roadside-forms/js/chunk-2d216257.6039ee9f.js"
+  },
+  {
+    "revision": "9a5094729c18e08dbd35",
+    "url": "/roadside-forms/js/chunk-4361ea0f.b5f380ee.js"
+  },
+  {
+    "revision": "983d921f6823f89ae478",
+    "url": "/roadside-forms/js/chunk-vendors.95d61d49.js"
+  },
+  {
+    "revision": "d614e0fcb8d493b145b2ccefdb520a31",
+    "url": "/roadside-forms/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/roadside-forms/robots.txt"
+  }
+]);
--- web_app/dist/service-worker.js
+++ web_app/dist/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.deaaa3eb7831f4b26576209076a00c24.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/roadside-forms/precache-manifest.33ce2134ba6557644a40b7240b410d4d.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 
 // The precaching code provided by Workbox
