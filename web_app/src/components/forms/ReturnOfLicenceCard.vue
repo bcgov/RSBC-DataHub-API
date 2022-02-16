09de4386ddca317c4bd9a2af047ec253622db90d
Jonathan Longe <jlonge@corebox.net>
2022-02-16 15:23:23 -0800

    DF-1512 - hide ReturnOfLicense component

--- web_app/src/components/forms/ReturnOfLicenceCard.vue
+++ web_app/src/components/forms/ReturnOfLicenceCard.vue
@@ -1,21 +1,20 @@
 <template>
-  <div>
-      <form-card title="Driver's Licence Disposition">
-        <form-row>
-          <radio-field id="licence_surrendered" fg_class="col-sm-12" :options='["Yes", "No"]'>Licence surrendered at roadside?</radio-field>
-        </form-row>
-        <form-row v-if="isLicenceSurrendered">
-          <radio-field id="return_of_licence" fg_class="col-sm-12" :options='["By mail", "Pickup in person"]'>How will licence be returned?</radio-field>
-        </form-row>
-        <form-row v-if="licencePickupInPerson && isLicenceSurrendered">
-          <type-ahead-field id="pickup_address" :suggestions="getArrayOfPickupLocations" fg_class="col-sm-6">
-            Pickup address and city
-          </type-ahead-field>
-        </form-row>
-      </form-card>
+  <!-- DISABLED WHILE WE WAIT FOR FORM TO BE ADJUSTED TO INCLUDE PLACE TO PRINT THESE FIELDS -->
+  <div v-if="false">
+    <form-card title="Driver's Licence Disposition">
+      <form-row>
+        <radio-field id="licence_surrendered" fg_class="col-sm-12" :options='["Yes", "No"]'>Licence surrendered at roadside?</radio-field>
+      </form-row>
+      <form-row v-if="isLicenceSurrendered">
+        <radio-field id="return_of_licence" fg_class="col-sm-12" :options='["By mail", "Pickup in person"]'>How will licence be returned?</radio-field>
+      </form-row>
+      <form-row v-if="licencePickupInPerson && isLicenceSurrendered">
+        <type-ahead-field id="pickup_address" :suggestions="getArrayOfPickupLocations" fg_class="col-sm-6">
+          Pickup address and city
+        </type-ahead-field>
+      </form-row>
+    </form-card>
   </div>
-
-
 </template>
 
 <script>
