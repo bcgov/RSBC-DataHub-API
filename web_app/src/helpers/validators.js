577eef7fe5ebdd818ed09bb82d45b2de4a0809db
Jonathan Longe <jlonge@corebox.net>
2022-05-06 14:00:46 -0700

    DF-1788 - fix email validation

--- web_app/src/helpers/validators.js
+++ web_app/src/helpers/validators.js
@@ -169,7 +169,8 @@ extend('phone', {
 extend('email', {
   validate(value) {
     let result = false;
-    const regexMatch = value.match("^\\w+@\\w+\\.\\w{2,}$")
+    const re = "^(([^<>()[\\]\\\\.,;:\\s@\"]+(\\.[^<>()[\\]\\\\.,;:\\s@\"]+)*)|(\".+\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$"
+    const regexMatch = value.match(re)
     if (Array.isArray(regexMatch)) {
        result = regexMatch[0] === value;
     }
