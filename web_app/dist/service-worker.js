14f0d89e1338c478198daf99760f2486c488cdc0
Jonathan Longe <jlonge@corebox.net>
2022-05-10 16:51:21 -0700

    update web_app to v0.3.5

--- web_app/dist/service-worker.js
+++ web_app/dist/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/roadside-forms/precache-manifest.6e16a39f27d6b700d597d67e6880e803.js", "/roadside-forms/workbox-v4.3.1/workbox-sw.js");
+importScripts("/roadside-forms/precache-manifest.fa8a8ea8cbdcf0c21b1d139a5d9b541f.js", "/roadside-forms/workbox-v4.3.1/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/roadside-forms/workbox-v4.3.1"});
 
 // The precaching code provided by Workbox
