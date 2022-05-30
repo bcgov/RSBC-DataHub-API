1c5ea92a92591c66427debcf66123abae4371099
Jonathan Longe <jlonge@corebox.net>
2022-05-29 18:08:13 -0700

    update web_app to v0.4.14

---
+++ web_app/dist/precache-manifest.af8d48fbcd940ce05ee56754553219f8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a1d1387d63bfa8f038a3563072d61e5",
+    "url": "/roadside-forms/assets/BCID_RoadSafetyBC_logo_transparent.png"
+  },
+  {
+    "revision": "5ce4da386a4eba0d6dab42b7f2bccf02",
+    "url": "/roadside-forms/assets/MV2634_102018_driver.png"
+  },
+  {
+    "revision": "99649d1f1ffade92e5110771bf0a69e3",
+    "url": "/roadside-forms/assets/MV2634_102018_police.png"
+  },
+  {
+    "revision": "84c56b7303097ba856dad1783b9a0311",
+    "url": "/roadside-forms/assets/MV2721_201502.png"
+  },
+  {
+    "revision": "7bbb24a042f52a431a88bce5a4caad00",
+    "url": "/roadside-forms/assets/MV2721_201502_appeal.png"
+  },
+  {
+    "revision": "1806cf03f5f0fb23651e55de8e313de8",
+    "url": "/roadside-forms/assets/MV2722_201502.png"
+  },
+  {
+    "revision": "3ffbcc270befb546fe7de5d9799621ce",
+    "url": "/roadside-forms/assets/MV2906_102018.png"
+  },
+  {
+    "revision": "1367ede801579ac591fd",
+    "url": "/roadside-forms/css/app.e4bee603.css"
+  },
+  {
+    "revision": "a8fa0150ee96ff3af6ed",
+    "url": "/roadside-forms/css/chunk-vendors.e42a3c01.css"
+  },
+  {
+    "revision": "365dad6d0ddc7cd28c617ba3a4751859",
+    "url": "/roadside-forms/index.html"
+  },
+  {
+    "revision": "1367ede801579ac591fd",
+    "url": "/roadside-forms/js/app.b35ab06a.js"
+  },
+  {
+    "revision": "a8fa0150ee96ff3af6ed",
+    "url": "/roadside-forms/js/chunk-vendors.8a1f8ec2.js"
+  },
+  {
+    "revision": "d614e0fcb8d493b145b2ccefdb520a31",
+    "url": "/roadside-forms/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/roadside-forms/robots.txt"
+  }
+]);
