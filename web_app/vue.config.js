d3705abb06a7ec45a689e06708df02adcd965938
Jonathan Longe <jlonge@corebox.net>
2022-06-03 16:58:20 -0700

    DF-1753 - automatically reload web app

--- web_app/vue.config.js
+++ web_app/vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/roadside-forms/' : '/',
+  publicPath: process.env.VUE_APP_PUBLIC_PATH,
   runtimeCompiler: true,
   pwa: {
     workboxPluginMode: "InjectManifest",
