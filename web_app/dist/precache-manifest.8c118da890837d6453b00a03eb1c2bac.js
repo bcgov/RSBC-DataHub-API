ab9cb712b8c43ed81eb8cc7f1d9562cf2845c38e
Jonathan Longe <jlonge@corebox.net>
2022-07-12 16:58:23 -0700

    update web_app to v0.5.5

--- web_app/dist/precache-manifest.8c118da890837d6453b00a03eb1c2bac.js
+++ web_app/dist/precache-manifest.8c118da890837d6453b00a03eb1c2bac.js
@@ -28,7 +28,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/roadside-forms/assets/MV2906_102018.png"
   },
   {
-    "revision": "1bb027edeb008e457006",
+    "revision": "03ab2fca617c666b391f",
     "url": "/roadside-forms/css/app.67d7e808.css"
   },
   {
@@ -36,12 +36,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/roadside-forms/css/chunk-vendors.ac5b10c9.css"
   },
   {
-    "revision": "5669d0878e23f274cd710365f21addba",
+    "revision": "31f3fe1ab73c9a8d697514e6a54f0b10",
     "url": "/roadside-forms/index.html"
   },
   {
-    "revision": "1bb027edeb008e457006",
-    "url": "/roadside-forms/js/app.184fc1d3.js"
+    "revision": "03ab2fca617c666b391f",
+    "url": "/roadside-forms/js/app.cb740eee.js"
   },
   {
     "revision": "3a5bf12dbc7c28e75de3",
--- web_app/dist/service-worker.js
+++ web_app/dist/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/roadside-forms/precache-manifest.6852b2cb5b20e493fa61f8b6198ba8fa.js", "/roadside-forms/workbox-v4.3.1/workbox-sw.js");
+importScripts("/roadside-forms/precache-manifest.8c118da890837d6453b00a03eb1c2bac.js", "/roadside-forms/workbox-v4.3.1/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/roadside-forms/workbox-v4.3.1"});
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {
