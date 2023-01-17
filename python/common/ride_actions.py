6698b66cdfc574ffa5f51769131a115f32ca749e
Amlan Chakladar <57117462+amlanc1@users.noreply.github.com>
2023-01-17 12:36:20 -0800

    updated

--- python/common/ride_actions.py
+++ python/common/ride_actions.py
@@ -4,5 +4,36 @@ import requests
 
 
 def app_accepted_event(**args):
-    logging.info("this is from ride function")
+    logging.info("this is from ride function app_accepted_event")
     logging.info(args)
+    #TODO: Call RIDE API endpoint
+    #TODO: For errors write to RabbitMQ
+    return True, args
+
+def disclosure_sent(**args):
+    logging.info("this is from ride function disclosure_sent")
+    logging.info(args)
+    # TODO: Call RIDE API endpoint
+    # TODO: For errors write to RabbitMQ
+    return True, args
+
+def evidence_submitted(**args):
+    logging.info("this is from ride function evidence_submitted")
+    logging.info(args)
+    # TODO: Call RIDE API endpoint
+    # TODO: For errors write to RabbitMQ
+    return True, args
+
+def payment_received(**args):
+    logging.info("this is from ride function payment_received")
+    logging.info(args)
+    # TODO: Call RIDE API endpoint
+    # TODO: For errors write to RabbitMQ
+    return True, args
+
+def review_scheduled(**args):
+    logging.info("this is from ride function review_scheduled")
+    logging.info(args)
+    # TODO: Call RIDE API endpoint
+    # TODO: For errors write to RabbitMQ
+    return True, args
