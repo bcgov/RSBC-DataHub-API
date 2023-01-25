9a57f7b9a54f0a0c2b0b7a484c6b3431c368c847
Amlan Chakladar <57117462+amlanc1@users.noreply.github.com>
2023-01-25 12:31:36 -0800

    updated

--- python/common/ride_actions.py
+++ python/common/ride_actions.py
@@ -5,92 +5,73 @@ import datetime
 
 
 def app_accepted_event(**args):
-    logging.info("this is from ride function new app_accepted_event")
-    logging.info(args)
-    #TODO: Call RIDE API endpoint
-    eventpayload = {}
-    eventpayload['typeofevent'] = 'app_accepted'
-    eventpayload['appacceptedpayload'] = []
-    payloadrecord = {}
-    payloadrecord["eventVersion"] = 1.0
+    try:
+        logging.info("this is from ride function new app_accepted_event")
+        logging.info(args)
+        #TODO: Call RIDE API endpoint
+        eventpayload = {}
+        eventpayload['typeofevent'] = 'app_accepted'
+        eventpayload['appacceptedpayload'] = []
+        payloadrecord = {}
+        payloadrecord["eventVersion"] = 1.0
 
-    # convert date time to string
-    tvalue = args['message']['event_date_time']
-    tformat = "%Y-%m-%dT%H:%M:%S.%f"
-    tformatted = datetime.datetime.strptime(tvalue, tformat)
-    format_string = "%Y-%m-%d %H:%M:%S"
-    dtstr = tformatted.strftime(format_string)
-    payloadrecord["eventDtm"] = dtstr
+        # convert date time to string
+        tvalue = args['message']['event_date_time']
+        tformat = "%Y-%m-%dT%H:%M:%S.%f"
+        tformatted = datetime.datetime.strptime(tvalue, tformat)
+        format_string = "%Y-%m-%d %H:%M:%S"
+        dtstr = tformatted.strftime(format_string)
+        payloadrecord["eventDtm"] = dtstr
 
-    payloadrecord["eventType"] = "app_accepted"
+        payloadrecord["eventType"] = "app_accepted"
 
-    # Get prohibition no
-    payloadrecord["prohibitionNo"] = args['message']['prohibition_review']['form']['prohibition-information'][
-        'prohibition-number-clean']
-    payloadrecord["prohibitionNoClean"] = args['message']['prohibition_review']['form']['prohibition-information'][
-        'prohibition-number-clean']
+        # Get prohibition no
+        payloadrecord["prohibitionNo"] = args['message']['prohibition_review']['form']['prohibition-information']['prohibition-number-clean']
+        payloadrecord["prohibitionNoClean"] = args['message']['prohibition_review']['form']['prohibition-information']['prohibition-number-clean']
 
-    # Get flags
-    payloadrecord["isAdp"] = bool(
-        args['message']['prohibition_review']['form']['prohibition-information']['control-is-adp'])
-    payloadrecord["isIrp"] = bool(
-        args['message']['prohibition_review']['form']['prohibition-information']['control-is-irp'])
-    payloadrecord["isUl"] = bool(
-        args['message']['prohibition_review']['form']['prohibition-information']['control-is-ul'])
-    payloadrecord["licenceNotSurrendered"] = (lambda x: False if (x == None or x == False) else True)(
-        args['message']['prohibition_review']['form']['prohibition-information']['licence-not-surrendered'])
-    payloadrecord["licenceLostOrStolen"] = (lambda x: False if (x == None or x == False) else True)(
-        args['message']['prohibition_review']['form']['prohibition-information']['licence-lost-or-stolen'])
-    payloadrecord["licenceNotIssued"] = (lambda x: False if (x == None or x == False) else True)(
-        args['message']['prohibition_review']['form']['prohibition-information']['licence-not-issued'])
+        # Get flags
+        payloadrecord["isAdp"] = (lambda x: False if (x==None or x=='false') else True)(args['message']['prohibition_review']['form']['prohibition-information']['control-is-adp'])
+        payloadrecord["isIrp"] = (lambda x: False if (x==None or x=='false') else True)(args['message']['prohibition_review']['form']['prohibition-information']['control-is-irp'])
+        payloadrecord["isUl"] = (lambda x: False if (x==None or x=='false') else True)(args['message']['prohibition_review']['form']['prohibition-information']['control-is-ul'])
+        payloadrecord["licenceNotSurrendered"] = (lambda x: False if (x == None or x == False or x=='false') else True)(args['message']['prohibition_review']['form']['prohibition-information']['licence-not-surrendered'])
+        payloadrecord["licenceLostOrStolen"] = (lambda x: False if (x == None or x == False or x=='false') else True)(args['message']['prohibition_review']['form']['prohibition-information']['licence-lost-or-stolen'])
+        payloadrecord["licenceNotIssued"] = (lambda x: False if (x == None or x == False or x=='false') else True)(args['message']['prohibition_review']['form']['prohibition-information']['licence-not-issued'])
 
-    # get dt of service
-    payloadrecord["dtOfService"] = args['message']['prohibition_review']['form']['prohibition-information'][
-        'date-of-service']
+        # get dt of service
+        payloadrecord["dtOfService"] = args['message']['prohibition_review']['form']['prohibition-information']['date-of-service']
 
-    # get role
-    payloadrecord["applicantRole"] = args['message']['prohibition_review']['form']['identification-information'][
-        'applicant-role']
+        # get role
+        payloadrecord["applicantRole"] = args['message']['prohibition_review']['form']['identification-information']['applicant-role']
 
-    # get applicant details
-    payloadrecord["applicantFirstNm"] = args['message']['prohibition_review']['form']['identification-information'][
-        'first-name-applicant']
-    payloadrecord["applicantLastNm"] = args['message']['prohibition_review']['form']['identification-information'][
-        'last-name-applicant']
-    payloadrecord["applicantPhoneNo"] = args['message']['prohibition_review']['form']['identification-information'][
-        'applicant-phone-number']
-    payloadrecord["applicantEmailAddr"] = args['message']['prohibition_review']['form']['identification-information'][
-        'applicant-email-address']
-    payloadrecord["applicantEmailConfirm"] = \
-    args['message']['prohibition_review']['form']['identification-information']['applicant-email-confirm']
+        # get applicant details
+        payloadrecord["applicantFirstNm"] = args['message']['prohibition_review']['form']['identification-information']['first-name-applicant']
+        payloadrecord["applicantLastNm"] = args['message']['prohibition_review']['form']['identification-information']['last-name-applicant']
+        payloadrecord["applicantPhoneNo"] = args['message']['prohibition_review']['form']['identification-information']['applicant-phone-number']
+        payloadrecord["applicantEmailAddr"] = args['message']['prohibition_review']['form']['identification-information']['applicant-email-address']
+        payloadrecord["applicantEmailConfirm"] = args['message']['prohibition_review']['form']['identification-information']['applicant-email-confirm']
 
-    # Get driver deails
-    payloadrecord["driverFirstNm"] = args['message']['prohibition_review']['form']['identification-information'][
-        'driver-first-name']
-    payloadrecord["driverLastNm"] = args['message']['prohibition_review']['form']['identification-information'][
-        'driver-last-name']
-    payloadrecord["driverInformationDriverDl"] = \
-    args['message']['prohibition_review']['form']['identification-information']['driver-bcdl']
-    payloadrecord["streetInformationStreetAddr"] = \
-    args['message']['prohibition_review']['form']['identification-information']['street-address']
-    payloadrecord["driverCityTown"] = args['message']['prohibition_review']['form']['identification-information'][
-        'control-driver-city-town']
-    payloadrecord["driverProvince"] = args['message']['prohibition_review']['form']['identification-information'][
-        'control-driver-province']
-    payloadrecord["driverPostalCde"] = args['message']['prohibition_review']['form']['identification-information'][
-        'control-driver-postal-code']
+        # Get driver deails
+        payloadrecord["driverFirstNm"] = args['message']['prohibition_review']['form']['identification-information']['driver-first-name']
+        payloadrecord["driverLastNm"] = args['message']['prohibition_review']['form']['identification-information']['driver-last-name']
+        payloadrecord["driverInformationDriverDl"] = args['message']['prohibition_review']['form']['identification-information']['driver-bcdl']
+        payloadrecord["streetInformationStreetAddr"] = args['message']['prohibition_review']['form']['identification-information']['street-address']
+        payloadrecord["driverCityTown"] = args['message']['prohibition_review']['form']['identification-information']['control-driver-city-town']
+        payloadrecord["driverProvince"] = args['message']['prohibition_review']['form']['identification-information']['control-driver-province']
+        payloadrecord["driverPostalCde"] = args['message']['prohibition_review']['form']['identification-information']['control-driver-postal-code']
 
-    # get applicant signatue info
-    payloadrecord["applicantSignature"] = args['message']['prohibition_review']['form']['consent-and-submission'][
-        'signature-applicant-name']
-    payloadrecord["applicantDtSigned"] = args['message']['prohibition_review']['form']['consent-and-submission'][
-        'date-signed']
+        # get applicant signatue info
+        payloadrecord["applicantSignature"] = args['message']['prohibition_review']['form']['consent-and-submission']['signature-applicant-name']
+        payloadrecord["applicantDtSigned"] = args['message']['prohibition_review']['form']['consent-and-submission']['date-signed']
 
-    eventpayload['appacceptedpayload'].append(payloadrecord)
-    endpoint = "https://api-be5301-dev.apps.silver.devops.gov.bc.ca/dfevents/appaccepted"
-    headers = {'ride-api-key': '7cb719a8-1d5a-4c65-9032-425e52355b07'}
-    response = requests.post(endpoint, json=eventpayload, verify=False,headers=headers)
-    print(response.json())
+        eventpayload['appacceptedpayload'].append(payloadrecord)
+        endpoint = "https://api-be5301-dev.apps.silver.devops.gov.bc.ca/dfevents/appaccepted"
+        headers = {'ride-api-key': '7cb719a8-1d5a-4c65-9032-425e52355b07'}
+        response = requests.post(endpoint, json=eventpayload, verify=False,headers=headers)
+        print(response.json())
+    except Exception as e:
+        logging.error('error in sending app_accepted event to RIDE')
+        logging.error(e)
+    
 
 
     #TODO: For errors write to RabbitMQ
@@ -106,14 +87,89 @@ def disclosure_sent(**args):
 def evidence_submitted(**args):
     logging.info("this is from ride function evidence_submitted")
     logging.info(args)
-    # TODO: Call RIDE API endpoint
+    try:
+        logging.info("this is from ride function new evidence_submitted")
+        logging.info(args)
+        # TODO: Call RIDE API endpoint
+        eventpayload = {}
+        eventpayload['typeofevent'] = 'evidence_submitted'
+        eventpayload['evidencesubmittedpayload'] = []
+        payloadrecord = {}
+        payloadrecord["eventVersion"] = 1.0
+
+        # convert date time to string
+        tvalue = args['message']['event_date_time']
+        tformat = "%Y-%m-%dT%H:%M:%S.%f"
+        tformatted = datetime.datetime.strptime(tvalue, tformat)
+        format_string = "%Y-%m-%d %H:%M:%S"
+        dtstr = tformatted.strftime(format_string)
+        payloadrecord["eventDtm"] = dtstr
+
+        payloadrecord["eventType"] = "evidence_submitted"
+
+        # Get prohibition no
+        payloadrecord["prohibitionNo"]=args.get('prohibition_number')
+        # payloadrecord["prohibitionNo"] = args['message']['prohibition_review']['form']['prohibition-information'][
+        #     'prohibition-number-clean']
+
+        eventpayload['evidencesubmittedpayload'].append(payloadrecord)
+        endpoint = "https://api-be5301-dev.apps.silver.devops.gov.bc.ca/dfevents/evidencesubmitted"
+        headers = {'ride-api-key': '7cb719a8-1d5a-4c65-9032-425e52355b07'}
+        response = requests.post(endpoint, json=eventpayload, verify=False, headers=headers)
+        print(response.json())
+    except Exception as e:
+        logging.error('error in sending evidence_submitted event to RIDE')
+        logging.error(e)
     # TODO: For errors write to RabbitMQ
     return True, args
 
 def payment_received(**args):
     logging.info("this is from ride function payment_received")
     logging.info(args)
-    # TODO: Call RIDE API endpoint
+    try:
+        logging.info(args)
+        payload = args.get('payload')
+        # TODO: Call RIDE API endpoint
+        eventpayload = {}
+        eventpayload['typeofevent'] = 'payment_received'
+        eventpayload['payrecvdpayload'] = []
+        payloadrecord = {}
+        payloadrecord["eventVersion"] = 1.0
+
+        # convert date time to string
+        # "eventDtm":"2021-12-27 15:40:45",
+        dt1 = datetime.datetime.now()
+        format_string = "%Y-%m-%d %H:%M:%S"
+        dtstr = dt1.strftime(format_string)
+        payloadrecord["eventDtm"] = dtstr
+
+        payloadrecord["eventType"] = "payment_received"
+
+        # Get prohibition no
+        payloadrecord["prohibitionNo"] = args.get('prohibition_number')
+
+        payloadrecord["recieptNo"] = payload['receipt_number']
+
+        payloadrecord["receiptAmt"] = payload['receipt_amount']
+
+        payloadrecord["transactionId"] = payload.get('transaction_id')
+
+        payloadrecord["paymentMethod"] = payload.get('payment_method')
+
+        payloadrecord["cardType"] = payload.get('cardtype')
+
+        receipt_datetime_object = args.get('receipt_date')
+
+        payloadrecord["receiptDtm"] = receipt_datetime_object.strftime(format_string)
+
+        eventpayload['payrecvdpayload'].append(payloadrecord)
+        endpoint = "https://api-be5301-dev.apps.silver.devops.gov.bc.ca/dfevents/paymentreceived"
+        headers = {'ride-api-key': '7cb719a8-1d5a-4c65-9032-425e52355b07'}
+        response = requests.post(endpoint, json=eventpayload, verify=False, headers=headers)
+        print(response.json())
+    except Exception as e:
+        logging.error('error in sending payment_received event to RIDE')
+        logging.error(e)
     # TODO: For errors write to RabbitMQ
     return True, args
 
