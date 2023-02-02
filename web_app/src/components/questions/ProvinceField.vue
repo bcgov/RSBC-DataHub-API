2ebc7cb79bb7a8de52ae71699455a296f9118125
victorchong <62109882+victorchong@users.noreply.github.com>
2023-02-02 13:16:20 -0800

    Cleaned up whitespace and formatting

--- web_app/src/components/questions/ProvinceField.vue
+++ web_app/src/components/questions/ProvinceField.vue
@@ -4,15 +4,7 @@
     <label v-if="show_label" :for="id"><slot></slot>
       <span v-if="required" class="text-danger"> *</span>
     </label>
-    <multiselect :value="getAttributeValue(path, id)"
-                   @input="updateProvince"
-                   :disabled="disabled || hasFormBeenPrinted"
-                   :id="id"
-                   tag-placeholder="That isn't an option"
-                   label="objectDsc"
-                   track-by="objectCd"
-                   placeholder="Search for a Province or State"
-                   :options="getArrayOfProvinces">
+    <multiselect :value="getAttributeValue(path, id)" @input="updateProvince" :disabled="disabled || hasFormBeenPrinted" :id="id" tag-placeholder="That isn't an option" label="objectDsc" track-by="objectCd" placeholder="Search for a Province or State" :options="getArrayOfProvinces">
     </multiselect>
     <div class="small text-danger">{{ errors[0] }}</div>
   </validation-provider>
@@ -20,44 +12,42 @@
 </template>
 
 <script>
-
-import FieldCommon from "@/components/questions/FieldCommon";
-import { mapGetters, mapMutations } from 'vuex';
-
-export default {
-  name: "ProvinceField",
-  props: {
-    defaultToBc: {
-      type: Boolean,
-      default: true
-    }
-  },
-  mixins: [FieldCommon],
-  mounted () {
-    if(this.defaultToBc) {
-      // set initial value to BC if value not set
-      if( ! this.getAttributeValue(this.path, this.id)) {
-        const bc = this.getArrayOfProvinces.filter(j => j.objectCd === "BC")[0]
-        this.$store.commit("updateFormField", { target: { id: this.id, path: this.path, value: bc}})
+  import FieldCommon from "@/components/questions/FieldCommon";
+  import { mapGetters, mapMutations } from 'vuex';
+  export default {
+    name: "ProvinceField",
+    props: {
+      defaultToBc: {
+        type: Boolean,
+        default: true
       }
-    }
-  },
-  computed: {
-    ...mapGetters(["getArrayOfProvinces", "getAttributeValue", "hasFormBeenPrinted"])
-  },
-  methods: {
-    ...mapMutations(["updateFormField"]),
-    updateProvince(event) {
-      console.log("updateProvince()", event)
-      const payload = {
-        "target": {
-          "path": this.path,
-          "id": this.id,
-          "value": event
+    },
+    mixins: [FieldCommon],
+    mounted () {
+      if(this.defaultToBc) {
+        // set initial value to BC if value not set
+        if( ! this.getAttributeValue(this.path, this.id)) {
+          const bc = this.getArrayOfProvinces.filter(j => j.objectCd === "BC")[0]
+          this.$store.commit("updateFormField", { target: { id: this.id, path: this.path, value: bc}})
         }
       }
-      this.updateFormField(payload)
+    },
+    computed: {
+      ...mapGetters(["getArrayOfProvinces", "getAttributeValue", "hasFormBeenPrinted"])
+    },
+    methods: {
+      ...mapMutations(["updateFormField"]),
+      updateProvince(event) {
+        console.log("updateProvince()", event)
+        const payload = {
+          "target": {
+            "path": this.path,
+            "id": this.id,
+            "value": event
+          }
+        }
+        this.updateFormField(payload)
+      }
     }
   }
-}
 </script>
