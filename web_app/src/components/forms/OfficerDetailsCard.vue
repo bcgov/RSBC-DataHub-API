09cf604e37ce63f06e612f890b15d5d089e97092
victorchong <62109882+victorchong@users.noreply.github.com>
2023-02-15 01:24:01 -0800

    Removed forced uppercase from agency name

--- web_app/src/components/forms/OfficerDetailsCard.vue
+++ web_app/src/components/forms/OfficerDetailsCard.vue
@@ -1,7 +1,7 @@
 <template>
   <form-card title="Officer">
     <form-row >
-      <text-field id="agency" :default_value="getCurrentUserObject.agency" fe_class="uppercase" fg_class="col-sm-6" :path="path" rules="required">Agency or RCMP Detachment</text-field>
+      <text-field id="agency" :default_value="getCurrentUserObject.agency" fg_class="col-sm-6" :path="path" rules="required">Agency or RCMP Detachment</text-field>
       <text-field id="badge_number" :default_value="getCurrentUserObject.badge_number" fg_class="col-sm-2" :path="path" rules="required">PRIME ID</text-field>
       <text-field id="officer_name" :default_value="getCurrentUserObject.last_name" fe_class="uppercase" fg_class="col-sm-4" :path="path" rules="required">Last Name of Peace Officer Serving Prohibition Notice</text-field>
     </form-row>
