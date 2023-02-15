6728c66363731908935f17298f2f987747d80971
victorchong <62109882+victorchong@users.noreply.github.com>
2023-02-14 23:39:40 -0800

    Cleaned up formatting

--- web_app/src/components/loading/LoadingResources.vue
+++ web_app/src/components/loading/LoadingResources.vue
@@ -3,26 +3,21 @@
     <div class="card-body">
         <div class="row small alert text-dark py-0 my-0">
         <div class="col-1 my-0">
-          <b-spinner v-if=" ! allResourcesLoaded" small></b-spinner>
+          <b-spinner v-if="!allResourcesLoaded" small></b-spinner>
         </div>
         <div class="col-11 text-left">Loading resources ...</div>
       </div>
     </div>
   </div>
 </template>
-
-
 <script>
-import {mapGetters} from "vuex";
-
-export default {
-  name: "LoadingResources",
-  computed: {
-  ...mapGetters(["allResourcesLoaded"])
+  import { mapGetters } from "vuex";
+  export default {
+    name: "LoadingResources",
+    computed: {
+      ...mapGetters([
+        "allResourcesLoaded"
+      ])
+    }
   }
-}
 </script>
-
-<style scoped>
-
-</style>
--- web_app/src/components/questions/FieldCommon.vue
+++ web_app/src/components/questions/FieldCommon.vue
@@ -1,69 +1,67 @@
 <script>
-
-import Multiselect from 'vue-multiselect'
-import { mapMutations, mapGetters } from 'vuex';
-
-export default {
-  name: "FieldCommon",
-  props: {
-    id: String,
-    disabled: {
-      type: Boolean,
-      default: false
-    },
-    show_label: {
-      type: Boolean,
-      default: true
-    },
-    visible: {
-      type: Boolean,
-      default: true
-    },
-    path: {
-      type: String
-    },
-    fg_class: String,
-    display_validation_errors: Boolean,
-    rules: {}
-  },
-  components: {Multiselect},
-  computed: {
-    attribute: {
-      get() {
-        return this.getAttributeValue(this.path, this.id)
-      },
-      set(value) {
-        const payload = {
-          target: {
-            path: this.path,
-            id: this.id,
-            value: value
-          }
-        }
-        return this.updateFormField(payload)
-      }
-    },
-    glowClass() {
-      if (this.errors) {
-        return "form-control border-danger"
-      }
-      return 'form-control'
-    },
-    isShowOptional() {
-      if(this.rules) {
-        return ! this.rules.includes('required')
-      }
-      return true
-    },
-    ...mapGetters([
+  import Multiselect from 'vue-multiselect'
+  import { mapMutations, mapGetters } from 'vuex';
+  export default {
+    name: "FieldCommon",
+    components: { Multiselect },
+    computed: {
+      ...mapGetters([
         "doesAttributeExist",
         "getAttributeValue",
-        "hasFormBeenPrinted",
         "hasFormBeenPrinted"
-    ])
-  },
-  methods: {
-    ...mapMutations(["updateFormField", "deleteFormField"])
+      ]),
+      attribute: {
+        get() {
+          return this.getAttributeValue(this.path, this.id)
+        },
+        set(value) {
+          const payload = {
+            target: {
+              path: this.path,
+              id: this.id,
+              value: value
+            }
+          }
+          return this.updateFormField(payload);
+        }
+      },
+      glowClass() {
+        if (this.errors) {
+          return "form-control border-danger";
+        }
+        return "form-control";
+      },
+      isShowOptional() {
+        if (this.rules) {
+          return ! this.rules.includes('required');
+        }
+        return true;
+      }
+    },
+    props: {
+      disabled: {
+        type: Boolean,
+        default: false
+      },
+      display_validation_errors: Boolean,
+      fe_class: String,
+      fg_class: String,
+      id: String,
+      path: {
+        type: String
+      },
+      rules: {},
+      show_label: {
+        type: Boolean,
+        default: true
+      },
+      visible: {
+        type: Boolean,
+        default: true
+      }
+    },
+    methods: {
+      ...mapMutations(["updateFormField", "deleteFormField"])
+    }
   }
-}
 </script>
