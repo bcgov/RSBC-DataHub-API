6f205de998119a5ccd02591210d98e52306f982a
Amlan Chakladar <57117462+amlanc1@users.noreply.github.com>
2023-03-16 11:15:51 -0700

    added

--- web_app/dist/precache-manifest.6139df3a310e5d5a43114495c99ed7ed.js
+++ web_app/dist/precache-manifest.6139df3a310e5d5a43114495c99ed7ed.js
@@ -28,7 +28,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/roadside-forms/assets/MV2906_102018.png"
   },
   {
-    "revision": "af9c9b1c80135051acf8",
+    "revision": "ea02063b4a3e4dc01650",
     "url": "/roadside-forms/css/app.37e3ec9e.css"
   },
   {
@@ -36,12 +36,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/roadside-forms/css/chunk-vendors.ac5b10c9.css"
   },
   {
-    "revision": "7b90dfdd7b9f7c6a04c4a24fdeb9751c",
+    "revision": "183d7e444a9e9c143cd40a4af2b2d206",
     "url": "/roadside-forms/index.html"
   },
   {
-    "revision": "af9c9b1c80135051acf8",
-    "url": "/roadside-forms/js/app.028742b2.js"
+    "revision": "ea02063b4a3e4dc01650",
+    "url": "/roadside-forms/js/app.5b0432e7.js"
   },
   {
     "revision": "c34e64b797aeada94a00",
