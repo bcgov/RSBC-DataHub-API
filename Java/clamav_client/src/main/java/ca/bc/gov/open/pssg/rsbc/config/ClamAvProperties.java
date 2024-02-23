6f9157e2535223548bb28d3053ade3dd05dc46db
Shaun Millar <shaun.millar@gov.bc.ca>
2024-02-22 17:50:32 -0800

    Added enviro variable support for ClamAV service

--- Java/clamav_client/src/main/java/ca/bc/gov/open/pssg/rsbc/config/ClamAvProperties.java
+++ Java/clamav_client/src/main/java/ca/bc/gov/open/pssg/rsbc/config/ClamAvProperties.java
@@ -2,7 +2,7 @@ package ca.bc.gov.open.pssg.rsbc.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "bcgov.clamav")
+@ConfigurationProperties(prefix = "clamav.client")
 public class ClamAvProperties {
 
     private String host = "localhost";
