ff37c1812059c6890124853f7186bf0c2f306bad
pabdi <pabdi1@gmail.com>
2024-03-25 09:39:17 -0700

    Fix build issues, adding some error handling, refactor form3, move secrets into server

--- web_app/next.config.mjs
+++ web_app/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '100mb',
+    },
+  },
+}
 
 export default nextConfig;
