a604dc0a7271de29d3bdff401fe05264bbd38d3a
Shaun Millar <shaun.millar@gov.bc.ca>
2025-06-18 10:20:09 -0700

    Added skeleton classes and methods

---
+++ Java/pdf-gen-service/src/main/java/ca/bc/gov/open/pssg/rsbc/pdf/service/AdobeOrdsService.java
@@ -0,0 +1,13 @@
+package ca.bc.gov.open.pssg.rsbc.pdf.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 
+ * Provides access to Adobe ORDs operations. 
+ * 
+ */
+@Service
+public class AdobeOrdsService {
+
+}
---
+++ Java/pdf-gen-service/src/main/java/ca/bc/gov/open/pssg/rsbc/pdf/service/AdobeReportServerService.java
@@ -0,0 +1,13 @@
+package ca.bc.gov.open.pssg.rsbc.pdf.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 
+ * Provides AEM Report Server. 
+ * 
+ */
+@Service
+public class AdobeReportServerService {
+
+}
