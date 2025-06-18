050d9865603ea2d19340c8f78e350ef0c3e57981
Shaun Millar <shaun.millar@gov.bc.ca>
2025-06-18 09:41:47 -0700

    Applied skeleton project

---
+++ Java/pdf-gen-service/src/main/java/ca/bc/gov/open/pssg/rsbc/pdf/PdfGeneratorServiceApplication.java
@@ -0,0 +1,13 @@
+package ca.bc.gov.open.pssg.rsbc.pdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfGeneratorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfGeneratorServiceApplication.class, args);
+	}
+
+}
---
+++ Java/pdf-gen-service/src/test/java/ca/bc/gov/open/pssg/rsbc/pdf/PdfGeneratorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.bc.gov.open.pssg.rsbc.pdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfGeneratorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
