0a551d5481104d5f7256c276729b2c87d8755199
Shaun Millar <shaun.millar@gov.bc.ca>
2025-06-22 20:20:32 -0700

    Added exceptions and xml utilities

---
+++ Java/pdf-gen-service/src/main/java/ca/bc/gov/open/pssg/rsbc/pdf/exception/UnsupportedFormTypeException.java
@@ -0,0 +1,13 @@
+package ca.bc.gov.open.pssg.rsbc.pdf.exception;
+
+public class UnsupportedFormTypeException extends Exception {
+	
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 3541860671744413070L;
+
+	public UnsupportedFormTypeException(String message) {
+        super(message);
+    }
+}
---
+++ Java/pdf-gen-service/src/main/java/ca/bc/gov/open/pssg/rsbc/pdf/exception/UnsupportedPayloadException.java
@@ -0,0 +1,13 @@
+package ca.bc.gov.open.pssg.rsbc.pdf.exception;
+
+public class UnsupportedPayloadException extends Exception {
+	
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 238227984223633039L;
+
+	public UnsupportedPayloadException(String message) {
+        super(message);
+    }
+}
