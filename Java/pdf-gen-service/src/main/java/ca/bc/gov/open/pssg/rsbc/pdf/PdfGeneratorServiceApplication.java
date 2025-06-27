d5ab03600f73bba4b603e62e264662f68198feb3
Shaun Millar <shaun.millar@gov.bc.ca>
2025-06-27 10:23:16 -0700

    Added retryable on the Adobe ORDS service.

--- Java/pdf-gen-service/src/main/java/ca/bc/gov/open/pssg/rsbc/pdf/PdfGeneratorServiceApplication.java
+++ Java/pdf-gen-service/src/main/java/ca/bc/gov/open/pssg/rsbc/pdf/PdfGeneratorServiceApplication.java
@@ -2,7 +2,9 @@ package ca.bc.gov.open.pssg.rsbc.pdf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
+@EnableRetry
 @SpringBootApplication
 public class PdfGeneratorServiceApplication {
 
