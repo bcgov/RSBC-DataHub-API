44137ea7820206db806b9f080223e858b28bd99e
Shaun Millar <shaun.millar@gov.bc.ca>
2025-07-05 22:56:43 -0700

    Completed the EmailAssembly service

--- Java/pdf-gen-service/src/main/java/ca/bc/gov/open/pssg/rsbc/pdf/component/MailItProperties.java
+++ Java/pdf-gen-service/src/main/java/ca/bc/gov/open/pssg/rsbc/pdf/component/MailItProperties.java
@@ -10,8 +10,43 @@ public class MailItProperties {
     private String username;
     private String password;
     private String url;
+    private String from; 
+    private String registry; 
+    private String bcc1;
+    private String bcc2; 
 
-    // Getters and Setters
+    public String getRegistry() {
+		return registry;
+	}
+
+	public void setRegistry(String registry) {
+		this.registry = registry;
+	}
+
+	public String getBcc1() {
+		return bcc1;
+	}
+
+	public void setBcc1(String bcc1) {
+		this.bcc1 = bcc1;
+	}
+
+	public String getBcc2() {
+		return bcc2;
+	}
+
+	public void setBcc2(String bcc2) {
+		this.bcc2 = bcc2;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
     public String getUsername() {
         return username;
     }
@@ -35,4 +70,5 @@ public class MailItProperties {
     public void setUrl(String url) {
         this.url = url;
     }
+    
 }
---
+++ Java/pdf-gen-service/src/main/java/ca/bc/gov/open/pssg/rsbc/pdf/service/EmailAssemblyService.java
@@ -0,0 +1,103 @@
+package ca.bc.gov.open.pssg.rsbc.pdf.service;
+
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.w3c.dom.Document;
+
+import ca.bc.gov.open.pssg.rsbc.pdf.component.MailItProperties;
+import ca.bc.gov.open.pssg.rsbc.pdf.models.EmailAttachment;
+import ca.bc.gov.open.pssg.rsbc.pdf.models.EmailContent;
+import ca.bc.gov.open.pssg.rsbc.pdf.models.EmailObject;
+import ca.bc.gov.open.pssg.rsbc.pdf.models.EmailRequest;
+import ca.bc.gov.open.pssg.rsbc.pdf.models.PDFRenderResponse;
+import ca.bc.gov.open.pssg.rsbc.pdf.utils.XmlUtilities;
+
+@Service
+public class EmailAssemblyService {
+
+	private MailItProperties props;
+
+	public EmailAssemblyService(MailItProperties props) {
+		this.props = props;
+	}
+
+	public MailItProperties getProps() {
+		return props;
+	}
+
+	/**
+	 * Generates an Email Request Object
+	 * 
+	 * @param resp
+	 * @param doc
+	 * @param noticeNumber
+	 * @param applicationForm
+	 * @param consentForm   // Optional base64 encoded consent form value
+	 * @return
+	 */
+	public EmailRequest getEmailRequest(PDFRenderResponse resp, Document doc, String noticeNumber,
+			String consentForm) {
+
+		EmailRequest er = new EmailRequest();
+		er.setSubject("Copy of Application form - Driving Prohibition " + noticeNumber + " Review");
+
+		// Set From
+		EmailObject eoFrom = new EmailObject();
+		eoFrom.setEmail(props.getFrom());
+		er.setFrom(eoFrom);
+
+		// Set To and BCCs
+		List<EmailObject> eoTos = new ArrayList<EmailObject>();
+		EmailObject eoTo1 = new EmailObject();
+		eoTo1.setEmail(XmlUtilities.getApplicantEmailAddress(doc));
+		eoTos.add(eoTo1);
+
+		if (null != props.getBcc1()) {
+			EmailObject eoTo2 = new EmailObject();
+			eoTo2.setEmail(props.getBcc1());
+			eoTos.add(eoTo2);
+		}
+
+		if (null != props.getBcc2()) {
+			EmailObject eoTo3 = new EmailObject();
+			eoTo3.setEmail(props.getBcc2());
+			eoTos.add(eoTo3);
+		}
+
+		er.setTo(eoTos);
+
+		List<EmailAttachment> attachments = new ArrayList<EmailAttachment>();
+
+		// Add prohibition review form attachment
+		if (resp.getPdf().length > 0) {
+			EmailAttachment attachment = new EmailAttachment();
+			attachment.setFilecontents(Base64.getEncoder().encodeToString(resp.getPdf()));
+			attachment.setFilename("prohibition_document.pdf");
+			attachments.add(attachment);
+		}
+
+		// Add consent form (if available) 
+		if ( null != consentForm) {
+			EmailAttachment attachment = new EmailAttachment();
+			attachment.setFilecontents(consentForm);
+			attachment.setFilename("consent.pdf");
+			attachments.add(attachment);
+		}
+
+		er.setAttachment(attachments);
+
+		EmailContent ec = new EmailContent();
+		ec.setType("text/html");
+
+		ec.setValue(resp.getEmailBody()); // html 
+
+		er.setContent(ec);
+
+		return er;
+
+	}
+
+}
